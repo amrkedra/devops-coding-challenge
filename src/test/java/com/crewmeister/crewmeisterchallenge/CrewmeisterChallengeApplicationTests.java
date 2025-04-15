0370e8283bc222b44922eaf8ce2c48efa64782f6
amrkedra <amrkedra.1993@gmail.com>
2025-04-15 20:05:39 +0200

    Created pipeline file

--- src/test/java/com/crewmeister/crewmeisterchallenge/CrewmeisterChallengeApplicationTests.java
+++ src/test/java/com/crewmeister/crewmeisterchallenge/CrewmeisterChallengeApplicationTests.java
@@ -3,11 +3,10 @@ package com.crewmeister.crewmeisterchallenge;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class CrewmeisterChallengeApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
-
+    @Test
+    void contextLoads() {
+    }
 }
